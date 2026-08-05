3af3c55c455e20b101e196a97558fb5d28bc691e
mstorange <ma.stutz@losinger-marazzi.ch>
2026-08-05 11:37:37 +0200

    Update code.py

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ from numpy import float64
 import matplotlib.pyplot as plt
 import httpx
 from io import StringIO
-from datetime import datetime
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 #from streamlit_folium import st_folium
 
